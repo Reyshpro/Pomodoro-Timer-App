7088497ecf9924ca66a2a6fc5db2c64b6e18c9a3
Reyyen Şehebi <reyyensehebi@gmail.com>
2026-01-11 15:08:46 +0300

    Initial React setup and basic layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Timer from './components/Timer'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Pomodoro App</h1>
+      <Timer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Timer.jsx
@@ -0,0 +1,9 @@
+function Timer() {
+  return (
+    <div>
+      <h2>25:00</h2>
+    </div>
+  )
+}
+
+export default Timer
